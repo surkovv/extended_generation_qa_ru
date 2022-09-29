f67b6b57c232f24b8a6bc6b65cf68bc7ab9d730a
Vyacheslav Surkov <surkov.vo@phystech.edu>
2022-09-29 03:28:42 +0300

    case2

--- preprocessing/src/generate.py
+++ preprocessing/src/generate.py
@@ -2,6 +2,8 @@ from collections import namedtuple
 from udapi.block.read.conllu import Conllu
 from io import StringIO
 import deeppavlov
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
 
 Instance = namedtuple('Instance', ['question', 'question_tree', 'answer', 'answer_tree'])
 
@@ -40,10 +42,14 @@ class LongAnswerGenerator:
         first_word = answer_tree.descendants[0]
         if first_word.upos == 'PROPN' and answer[0].upper() != answer[0] and len(first_word.form) > 1:
             answer = answer[0].upper() + answer[1:]
-            return answer, self.get_tree(*self.parser([answer]))
+            x = answer_tree.descendants[0].form
+            answer_tree.descendants[0].form = x[0].upper() + x[1:]
+            return answer, answer_tree 
         elif first_word.upos != 'PROPN' and answer[0].lower() != answer[0]:
             answer = answer[0].lower() + answer[1:]
-            return answer, self.get_tree(*self.parser([answer]))
+            x = answer_tree.descendants[0].form
+            answer_tree.descendants[0].form = x[0].lower() + x[1:]
+            return answer, answer_tree 
         return answer, answer_tree 
 
     def generate_long_answer(self, question, question_tree, answer, answer_tree):
@@ -54,12 +60,14 @@ class LongAnswerGenerator:
             answer = answer[0].upper() + answer[1:]
         if answer[-1] != '.':
             answer += '.'
+        answer = answer.replace(' ,', ',').replace(' .', '.').replace('„ ', '„').replace(' “', '“').replace(' :', ':').replace('( ', '(').replace(' )', ')')
         return answer
 
     def generate_one(self, question, answer):
         question = self.preprocess_question(question)
         answer = self.preprocess_answer(answer)
         question_tree = self.get_tree(*self.parser([question]))
+        question_tree.descendants[-1].remove()
         answer_tree = self.get_tree(*self.parser([answer]))
         answer, answer_tree = self.preprocess_with_tree(answer, answer_tree)
         question, question_tree = self.preprocess_with_tree(question, question_tree)
@@ -86,12 +94,22 @@ class CommonGenerator:
 
 
 def get_root(tree):
+    if tree.udeprel == 'root':
+        return tree
     return tree.children[0]
+
+def get_children(node, udeprel):
+    res = []
+    for child in node.children:
+        if child.udeprel == udeprel:
+            res.append(child)
+    return res
+
 
 @handler
 class Case1Handler:
     def check(self, question, question_tree):
-        root = question_tree.children[0]
+        root = get_root(question_tree)
         if root.form.lower() in ['кто', 'что']:
             return True
         return False
@@ -107,15 +125,13 @@ class Case1Handler:
 
         idx = max(id1, id2)
 
-        if idx == -1:
-            return answer, answer_tree
-
-        answer = answer[idx+1:]
-        for node in answer_tree.descendants:
-            tobreak = node.form in '-—'
-            node.remove(children='rehang')
-            if tobreak:
-                break
+        if idx != -1:
+            answer = answer[idx+1:]
+            for node in answer_tree.descendants:
+                tobreak = node.form in '-—'
+                node.remove(children='rehang')
+                if tobreak:
+                    break
         
         if answer_tree.descendants[0].form == 'это':
             answer_tree.descendants[0].remove(children='rehang')
@@ -138,7 +154,6 @@ class Case1Handler:
         long_answer = answer
         
         this_node = self.find_this(get_root(question_tree))
-        question_tree.descendants[-1].remove()
         if this_node:
             # Что такое/Кто такой
             root = get_root(question_tree)
@@ -164,5 +179,174 @@ class Case1Handler:
                     nsubj = node
                     break
             nsubj.shift_before_subtree(aux)
+            long_answer = question_tree.compute_text()
+        return True, long_answer
+
+
+class Reordable:
+    def can_reorder(self, root):
+        prefix = "в отличии"
+        text = root.compute_text().lower() 
+        if len(text) >= len(prefix) and text[:len(prefix)] == prefix:
+            return False
+        return True
+
+    def reorder_node(self, root):
+        # ORDER: nsubj [advmod, ...] root xcomp [obj, ...] [iobj, ...] [obl, ...]
+        
+        if (not self.can_reorder(root)):
+            return
+
+        before, after = [], []
+        before.extend(get_children(root, 'nsubj'))
+        before.extend(get_children(root, 'advmod'))
+        after.extend(get_children(root, 'xcomp'))
+        after.extend(get_children(root, 'obj'))
+        after.extend(get_children(root, 'iobj'))
+        after.extend(get_children(root, 'obl'))
+
+        for node in before[::-1]:
+            node.shift_before_subtree(root)
+
+        for node in after[::-1]:
+            node.shift_after_node(root)
+
+        for xcomp in get_children(root, 'xcomp'):
+            self.reorder_node(xcomp)
+
+    def reorder_tree(self, tree):
+        self.reorder_node(get_root(tree))
+
+
+class RootTrimmer:
+    def trim(self, question_tree, answer_tree):
+        # WARN: do not harmonize after trimming
+        if get_root(question_tree).form.lower() == get_root(answer_tree).form.lower():
+            get_root(answer_tree).remove(children='rehang')
+            return True
+        return False
+
+class Harmonizer:
+    case_table = {
+        'Nom': 'nomn',
+        'Gen': 'gent',
+        'Par': 'gent',
+        'Dat': 'datv',
+        'Acc': 'accs',
+        'Loc': 'loct',
+        'Ins': 'ablt',
+        'Voc': 'voct',
+        '': 'nomn'
+    }
+    number_table = {
+        'Sing': 'sing',
+        'Plur': 'plur',
+        '': 'sing'
+    }
+    
+    def harmonize_verb(self, qword_node, question_tree, answer_tree):
+        root = get_root(question_tree)
+        if root.upos != 'VERB' or qword_node.feats['Case'] != 'Nom':
+            return
+        number = ''
+        aroot = get_root(answer_tree)
+        if qword_node.lemma == 'какой':
+            number = aroot.feats['Number']
+        elif qword_node.lemma in ['кто', 'что', 'чем']:
+            number = aroot.feats['Number']
+        if len(get_children(aroot, 'conj')) > 0:
+            number = 'Plur'
+        if root.feats['Number'] == '':
+            number = ''
+        if root.feats['Number'] != number:
+            params = None
+            if number != '':
+                params = {self.number_table[number]}
+            if params is not None:
+                new_word = morph.parse(root.form)[0].inflect(params).word
+                root.form = new_word
+
+    def harmonize_noun(self, qword_node, question_tree, answer_tree):
+        if answer_tree.descendants[0].form.lower() in ['как']:
+            return
+        root = get_root(question_tree)
+        aroot = get_root(answer_tree)
+        if aroot.upos not in ['NOUN', 'PNOUN', 'PROPN']:
+            return
+        case, number = '', ''
+        if qword_node.lemma == 'какой':
+            case = qword_node.feats['Case']
+            number = qword_node.feats['Number']
+        elif qword_node.lemma in ['кто', 'что', 'чем']:
+            case = qword_node.feats['Case']
+            number = aroot.feats['Number']
+
+        if aroot.feats['Case'] != case or aroot.feats['Number'] != number:
+            params = None
+            if case != '' and number != '':
+                params = {self.case_table[case], self.number_table[number]}
+            elif number != '':
+                params = {self.number_table[number]}
+            elif case != '':
+                params = {self.case_table[case]}
+            if params is not None:
+                parsers = list(filter(lambda x: x.tag.POS == 'NOUN', morph.parse(aroot.form)))
+                if len(parsers) > 0:
+                    new_word = parsers[0].inflect(params).word
+                    aroot.form = new_word
+
+        preps_qword = get_children(qword_node, 'case')
+        preps = get_children(aroot, 'case')
+        if len(preps_qword) > 0 and len(preps) == 0:
+            prep_node = aroot.create_child(deprel='case')
+            prep_node.form = prep_node.lemma = preps_qword[0].form
+            prep_node.upos = 'ADP'
+            prep_node.shift_before_subtree(aroot)
+
+    def harmonize(self, qword_node, question_tree, answer_tree):
+        self.harmonize_verb(qword_node, question_tree, answer_tree)
+        self.harmonize_noun(qword_node, question_tree, answer_tree)
+
+
+@handler
+class Case2Handler(RootTrimmer, Harmonizer, Reordable):
+    def find_qword(self, question_tree):
+        root = get_root(question_tree)
+        if len(root.children) > 0:
+            clause = root.children[0]
+            for word in clause.descendants(add_self=True):
+                if word.lemma in ['кто', 'что', 'чем']:
+                    return word
+        return None
+
+    def check(self, question, question_tree):
+        return self.find_qword(question_tree) != None
+
+    def generate(self, question, question_tree, answer, answer_tree):
+        if not self.check(question, question_tree):
+            return False, None
+        if len(answer_tree.descendants) >= 5 and \
+        get_root(answer_tree).upos == 'VERB':
+            return True, answer
+        if not self.trim(question_tree, answer_tree):
+            qword_node = self.find_qword(question_tree)
+            self.harmonize(qword_node, question_tree, answer_tree)
+
+        qword_node = self.find_qword(question_tree)
+        if qword_node.form.lower() == 'что' and answer_tree.descendants[0].form.lower() == 'что':
+            qword_node.form = answer_tree.compute_text()
+            for c in qword_node.children: 
+                c.remove()
+            qword_node.shift_after_subtree(question_tree.descendants[-1])
+            root = get_root(question_tree)
+            nsubj = get_children(root, 'nsubj')[0]
+
+            nsubj.shift_before_subtree(root)
+            long_answer = question_tree.compute_text()
+        else:
+            qword_node.form = answer_tree.compute_text()
+            for c in qword_node.children: 
+                c.remove()
+            self.reorder_tree(question_tree)
             long_answer = question_tree.compute_text()
         return True, long_answer
